3f75d27e2b1d7559bc463d0da8664c953aa40086
Thomas Nix <tnix2@uncc.edu>
2017-09-30 12:33:42 -0400

    Added menu bar to frame.

--- FlashCardBuilder/src/com/thomasnix/flashcards/FlashCardBuilder.java
+++ FlashCardBuilder/src/com/thomasnix/flashcards/FlashCardBuilder.java
@@ -4,6 +4,8 @@ import sun.reflect.generics.scope.Scope;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
 public class FlashCardBuilder {
@@ -57,20 +59,47 @@ public class FlashCardBuilder {
         JLabel jLabelQuestion = new JLabel("Question");
         JLabel jLabelAnswer = new JLabel("Answer");
 
-        //Adding components
+        //// JMenuBar and JMenu / JMenuItem for File
+        JMenuBar jMenuBar = new JMenuBar();
+        JMenu jMenuFile = new JMenu("File");
+        JMenuItem jMenuItemNew = new JMenuItem("New");
+        JMenuItem jMenuItemSave = new JMenuItem("Save");
+
+        //// Menu Items
+        jMenuItemNew.addActionListener(new NewMenuItemListener());
+        jMenuItemSave.addActionListener(new SaveMenuItemListener());
+
+        // Add items to menu
+        jMenuFile.add(jMenuItemNew);
+        jMenuFile.add(jMenuItemSave);
+
+        // Add menu bar to JMenu
+        jMenuBar.add(jMenuFile);
+
+        // Adding components
         mainPanel.add(jLabelQuestion);
         mainPanel.add(jScrollPanelQuestion);
         mainPanel.add(jLabelAnswer);
         mainPanel.add(jScrollPanelAnswer);
         mainPanel.add(jButtonNext);
 
+        // Action listeners
 
-        // Add constraints to the pane
+        //// Button
+        jButtonNext.addActionListener(new NextCardListener());
+
+        // Add menu to the frame
+        jFrame.setJMenuBar(jMenuBar);
+
+        // Add constraints to the frame
         jFrame.getContentPane().add(BorderLayout.CENTER, mainPanel);
         jFrame.setSize(500,600);
         jFrame.setVisible(true);
     }
 
+    /*
+        Main - Run program and Swing utility (Flash card builder)
+     */
     public static void main(String[] args){
 
         // Run FlashCardBuilder constructor - Set up application and frame
@@ -80,5 +109,35 @@ public class FlashCardBuilder {
                 new FlashCardBuilder();
             }
         });
+    }
+
+    /*
+        Listener for the Next button
+     */
+    class NextCardListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e){
+            System.out.println("Button clicked!");
+        }
+    }
+
+    /*
+        Listener for the New menu item
+     */
+    class NewMenuItemListener implements ActionListener{
+        @Override
+        public void actionPerformed(ActionEvent e){
+            System.out.println("New menu item clicked!");
+        }
+    }
+
+    /*
+        Listener for the Save menu item
+     */
+    class SaveMenuItemListener implements ActionListener{
+        @Override
+        public void actionPerformed(ActionEvent e){
+            System.out.println("Save menu item clicked!");
+        }
     }
 }
