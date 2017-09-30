d3c9816cfa4dabf3ad934795d72012264d2a121f
Thomas Nix <tnix2@uncc.edu>
2017-09-30 11:32:10 -0400

    Initialized Flash Card project.

---
+++ FlashCardBuilder/src/com/thomasnix/flashcards/FlashCardBuilder.java
@@ -0,0 +1,12 @@
+package com.thomasnix.flashcards;
+
+public class FlashCardBuilder {
+
+    FlashCardBuilder(){
+        // Build UI here
+    }
+
+    public static void main(String[] args){
+
+    }
+}
