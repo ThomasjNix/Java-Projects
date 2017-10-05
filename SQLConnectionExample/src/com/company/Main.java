0f61c7b0c49308ae2ed6a6c823b9be8aff5873b1
Thomas Nix <tnix2@uncc.edu>
2017-10-05 15:57:53 -0400

    Added example of reading from a SQL database.

--- SQLConnectionExample/src/com/company/Main.java
+++ SQLConnectionExample/src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 import com.company.Helper.DBHandler;
 import com.mysql.jdbc.PreparedStatement;
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class Main {
@@ -12,8 +13,13 @@ public class Main {
     // Allows creation of SQL queries
     static private PreparedStatement preparedStatement;
 
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        writeToDB();
+        readFromDB();
 
+    }
+
+    public static void writeToDB() throws SQLException, ClassNotFoundException {
         // Get instance of database connection
         dbHandler = new DBHandler();
         connection = dbHandler.getConnection();
@@ -26,13 +32,24 @@ public class Main {
 
         // Create prepared statement for the connection, set values
         preparedStatement = (PreparedStatement) connection.prepareStatement(insert);
-        preparedStatement.setString(1, "Jansen");
-        preparedStatement.setString(2, "Phillips");
-        preparedStatement.setString(3, "JPhil323");
-        preparedStatement.setString(4, "123 Make Believe St");
-        preparedStatement.setInt(5, 35);
+        preparedStatement.setString(1, "Test");
+        preparedStatement.setString(2, "User");
+        preparedStatement.setString(3, "TestUsername");
+        preparedStatement.setString(4, "9999 Test st");
+        preparedStatement.setInt(5, 21);
 
         // Execute update
         preparedStatement.executeUpdate();
+    }
+
+    public static void readFromDB() throws SQLException {
+        String query = "SELECT * FROM user";
+
+        PreparedStatement preparedStatement = (PreparedStatement) connection.prepareStatement(query);
+        ResultSet resultSet = preparedStatement.executeQuery();
+
+        while (resultSet.next()){
+            System.out.println("Names: " + resultSet.getString("firstname") + " " + resultSet.getString("lastname"));
+        }
     }
 }
